82f1a35cfac60dbd3d850a9ea8261cf9732a7180
@ruachofeliyahu <ishaqdewolf@gmail.com>
2025-07-16 04:01:47 -0700

    Update stemmer.js

--- src/utils/stemmer.js
+++ src/utils/stemmer.js
@@ -1,1 +1,20 @@
+// src/utils/stemmer.js
+export function stemWord(word) {
+  const rules = [
+    { suffix: 'ies', replacement: 'y' },
+    { suffix: 'es', replacement: '' },
+    { suffix: 'ed', replacement: '' },
+    { suffix: 'ing', replacement: '' },
+    { suffix: 's', replacement: '' }
+  ];
+  
+  let stemmed = word.toLowerCase();
+  for (const rule of rules) {
+    if (stemmed.endsWith(rule.suffix)) {
+      stemmed = stemmed.slice(0, -rule.suffix.length) + rule.replacement;
+      break;
+    }
+  }
+  return stemmed;
+}
 
